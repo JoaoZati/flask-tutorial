a156b7f900a849f027924d28d4b064e132a09ff0
JoaoZati <joaozati@gmail.com>
2021-10-01 19:32:25 -0300

    - Criando uma main page

--- tutorial.py
+++ tutorial.py
@@ -2,4 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.run()
+
+@app.route("/")
+def home():
+    return "Hello! this is the main page <h1>HELLO</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
