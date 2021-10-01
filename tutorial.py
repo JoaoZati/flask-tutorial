69fc9ed713b85c83a047a7d08e67c62caa160d10
JoaoZati <joaozati@gmail.com>
2021-10-01 19:43:08 -0300

    - Renderizando o primeiro template html com o render_template do flask

--- tutorial.py
+++ tutorial.py
@@ -1,11 +1,11 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello! this is the main page <h1>HELLO</h1>"
+    return render_template('index.html')
 
 
 @app.route('/<name>')
