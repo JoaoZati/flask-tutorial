d259d8d4b867c1bac3764de313df72f38111f561
JoaoZati <joaozati@gmail.com>
2021-10-02 18:45:53 -0300

    - O começo para sqlite e banco de dados no Users - Criado view para ver todos os emails cadastrados

--- tutorial.py
+++ tutorial.py
@@ -15,7 +15,7 @@ class Users(db.Model):
     _id = db.Column('id', db.Integer, primary_key=True)
     name = db.Column('name', db.String(100))
     email = db.Column('email', db.String(100))
-    
+
     def __init__(self, name, email):
         self.email = email
         self.name = name
@@ -35,6 +35,9 @@ def user():
         if request.method == 'POST':
             email = request.form['email']
             session['email'] = email
+            found_user = Users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
             flash('Email was saved!')
         else:
             if 'email' in session:
@@ -56,6 +59,15 @@ def login():
         session.permanent = True
         user = request.form['nm']
         session['user'] = user
+
+        found_user = Users.query.filter_by(name=user).first()
+        if found_user:
+            session['email'] = found_user.email
+        else:
+            urs = Users(user, "")
+            db.session.add(urs)
+            db.session.commit()
+
         flash('Login successful')
         return redirect(url_for('user'))
     if 'user' in session.keys():
@@ -72,6 +84,11 @@ def logout():
     session.pop("user", None)
     session.pop("email", None)
     return redirect(url_for("login"))
+
+
+@app.route("/view")
+def view():
+    return render_template('view.html', values=Users.query.all())
 
 
 if __name__ == '__main__':
