40baf61224078b9f0c1267b3d1d1e2d40efa9b56
JoaoZati <joaozati@gmail.com>
2021-10-02 17:53:39 -0300

    - Importando flask - Adicionando container fluid in base html, make it a litle bit nicer.

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'hello'
